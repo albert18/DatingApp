67ccdabc95a555b6e6dc97a2f58f64a74b1fc5e4
albert18 <caps18@hotmail.com>
2018-09-27 07:36:59 -0400

    push

---
+++ DatingApp/DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _repo;
+
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+        }
+    }
+}
