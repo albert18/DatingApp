e390763bb42e2de1212ea13bf42ac406590b51e3
albert18 <caps18@hotmail.com>
2018-09-27 20:09:40 -0400

    Finish Security

--- DatingApp/DatingApp.API/Data/AuthRepository.cs
+++ DatingApp/DatingApp.API/Data/AuthRepository.cs
@@ -6,7 +6,7 @@ namespace DatingApp.API.Data
 {
     public class AuthRepository : IAuthRepository
     {
-                private readonly DataContext _context;
+        private readonly DataContext _context;
         public AuthRepository(DataContext context)
         {
             _context = context;
