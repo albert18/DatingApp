1405c0f46c32644d2f485a577a9dcfbaae0637c8
albert18 <caps18@hotmail.com>
2018-10-09 09:05:33 -0400

    Injecting the angular services in components

--- DatingApp/DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp/DatingApp-SPA/src/app/nav/nav.component.ts
@@ -16,10 +16,10 @@ export class NavComponent implements OnInit {
 
   login() {
     //  console.log(this.model);
-    // this.authService.login(this.model).subscribe(next => {
-    //   console.log('Logged in successfully');
-    // }, error => {
-    //   console.log('Failed to login');
-    // });
+    this.authService.login(this.model).subscribe(next => {
+      console.log('logged in successfully');
+    }, error => {
+      console.log('failed to login');
+    });
   }
 }
