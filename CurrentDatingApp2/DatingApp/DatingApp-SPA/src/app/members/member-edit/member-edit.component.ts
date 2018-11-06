4e9490555aec2a2225c8f87c2d846ce78087273c
albert18 <caps18@hotmail.com>
2018-11-05 19:31:07 -0500

    Template section 10

--- CurrentDatingApp2/DatingApp/DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ CurrentDatingApp2/DatingApp/DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { User } from '../../_models/user';
 import { ActivatedRoute } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { AlertifyService } from '../../_services/alertify.service';
 
 @Component({
   selector: 'app-member-edit',
@@ -8,13 +10,26 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm: NgForm;
   user: User;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.user = data['user'];
     });
+  }
+
+  updateUser() {
+    console.log(this.user);
+    this.alertify.success('Profile Updated!');
+    this.editForm.reset(this.user);
+    // this.userService.updateUser(this.authService.decodedToken.nameid, this.user).subscribe(next => {
+    //   this.alertify.success('Profile updated successfully');
+    //   this.editForm.reset(this.user);
+    // }, error => {
+    //   this.alertify.error(error);
+    // });
   }
 }
