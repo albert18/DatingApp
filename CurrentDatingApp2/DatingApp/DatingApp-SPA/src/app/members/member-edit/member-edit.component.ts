787992c288089151d8655ce97fe54bfb036b62bc
albert18 <caps18@hotmail.com>
2018-11-07 07:54:46 -0500

    updating Resources section 10

--- CurrentDatingApp2/DatingApp/DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ CurrentDatingApp2/DatingApp/DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
 import { User } from '../../_models/user';
 import { ActivatedRoute } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { AlertifyService } from '../../_services/alertify.service';
+import { UserService } from '../../_services/user.service';
+import { AuthService } from '../../_services/auth.service';
 
 @Component({
   selector: 'app-member-edit',
@@ -12,8 +14,15 @@ import { AlertifyService } from '../../_services/alertify.service';
 export class MemberEditComponent implements OnInit {
   @ViewChild('editForm') editForm: NgForm;
   user: User;
+  @HostListener('window:beforeunload', ['$event'])
+  unloadNotification($event: any) {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
 
-  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
+  constructor(private route: ActivatedRoute, private alertify: AlertifyService,
+    private userService: UserService, private authService: AuthService) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
@@ -22,14 +31,14 @@ export class MemberEditComponent implements OnInit {
   }
 
   updateUser() {
-    console.log(this.user);
-    this.alertify.success('Profile Updated!');
-    this.editForm.reset(this.user);
-    // this.userService.updateUser(this.authService.decodedToken.nameid, this.user).subscribe(next => {
-    //   this.alertify.success('Profile updated successfully');
-    //   this.editForm.reset(this.user);
-    // }, error => {
-    //   this.alertify.error(error);
-    // });
+    // console.log(this.user);
+    // this.alertify.success('Profile Updated!');
+    // this.editForm.reset(this.user);
+    this.userService.updateUser(this.authService.decodedToken.nameid, this.user).subscribe(next => {
+      this.alertify.success('Profile updated successfully');
+      this.editForm.reset(this.user);
+    }, error => {
+      this.alertify.error(error);
+    });
   }
 }
